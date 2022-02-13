94178fe758e21af646c8c5654bf8687cd6dccd72
Chase Harrison <chaseh88@gmail.com>
2022-02-13 04:16:56 -0600

    Add third web dependencies and app setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,24 @@
 import { AppProps } from 'next/app';
+import { ThirdwebWeb3Provider } from '@3rdweb/hooks';
+
+const supportedChainIds: number[] = [4];
+const connectors = {
+  injected: {}
+}
 
 /**
  * Main '_app' entry point to be shown on every page
  */
 const App = ({ Component, pageProps }: AppProps) => (
   <>
-    <div className='container'>
-      <Component {...pageProps} />
-    </div>
+    <ThirdwebWeb3Provider
+      supportedChainIds={supportedChainIds}
+      connectors={connectors}
+    >
+      <div className='container'>
+        <Component {...pageProps} />
+      </div>
+    </ThirdwebWeb3Provider>
   </>
 )
 
