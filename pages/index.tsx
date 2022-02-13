a5ab15c8308732e4d3d29316c75f45e5735bda20
Chase Harrison <chaseh88@gmail.com>
2022-02-13 04:18:17 -0600

    Adding styled components and test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
 import { FC } from 'react';
+import styled from 'styled-components';
+
+const TestStyle = styled.h1`
+  color: #000080;
+`;
 
 const Home: FC = () => {
   return (
     <div>
-      Next App
+      <TestStyle>
+        Next App
+      </TestStyle>
     </div>
   )
 }
