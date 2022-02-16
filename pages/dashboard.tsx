bca14f3179f605c0fe394d2e372e6733b5e5f15e
Chase Harrison <chaseh88@gmail.com>
2022-02-15 19:52:59 -0600

    cleanup

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -2,19 +2,12 @@ import { FC } from 'react';
 
 // Components
 import { CoinList } from 'components/Dashboard/CoinList';
-import { Input } from 'components/General/Input';
 
 const Home: FC = () => {
 
   return (
     <div>
       <CoinList />
-      <Input
-        placeholder='Search'
-        onChange={(e) => {
-          console.log(e.target.value)
-        }}
-      />
     </div>
   )
 }
